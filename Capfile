46574491197f18a47253674c3b6d5d27ee592a84
gopal1726 <gopalmyself@gmail.com>
2022-01-30 18:15:34 +0530

    update config file

--- Capfile
+++ Capfile
@@ -1,29 +1,13 @@
-# require "capistrano/setup"
-# require "capistrano/deploy"
-# require "capistrano/rails"
-# require "capistrano/puma"
-# install_plugin Capistrano::Puma
-# install_plugin Capistrano::Puma::Daemon
-# require "capistrano/yarn"
-# require "capistrano/bundler"
-# require "capistrano/scm/git"
-# install_plugin Capistrano::SCM::Git
-# install_plugin Capistrano::Puma::Nginx
-
-# Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
-
-# Load DSL and Setup Up Stages
-require 'capistrano/setup'
-require 'capistrano/deploy'
-require "capistrano/scm/git"
-install_plugin Capistrano::SCM::Git
-require 'capistrano/rails'
-require 'capistrano/bundler'
-require 'capistrano/rvm'
-require 'capistrano/puma'
+require "capistrano/setup"
+require "capistrano/deploy"
+require "capistrano/rails"
+require "capistrano/puma"
 install_plugin Capistrano::Puma
 install_plugin Capistrano::Puma::Daemon
+require "capistrano/yarn"
+require "capistrano/bundler"
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+install_plugin Capistrano::Puma::Nginx
 
-
-# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,25 @@
 # Change these
-
-
 set :repo_url,        "git@github.com:Gopal221/bromo.git"
 set :application,     'bromo'
 set :user,            'bromo'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-
-# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
+set :stages,          ["staging", "production"]
+set :default_stage,   "production"
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :puma_env,        fetch(:rack_env, fetch(:rails_env, 'staging'))
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-
-## Defaults:
-# set :scm,           :git
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
-set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -54,7 +40,7 @@ namespace :deploy do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
-        # exit
+        exit
       end
     end
   end
@@ -77,6 +63,5 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart
-
 end
+
